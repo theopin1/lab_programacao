d46a07a0196c359e0c6dc428a4997b07f01f63be
theopin1 <142269695+theopin1@users.noreply.github.com>
2023-08-30 08:52:42 -0300

    Create contas.c

---
+++ contas.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+int x;
+    printf("Escreva um numero qualquer: ");
+    scanf("%d", &x);
+    
+    printf("\nO triplo desse numero é: %d\n", x * 3 );
+    printf("O quadrado desse numero é: %d\n", x * 2 );
+    printf("O quadrado desse numero é: %d\n", x / 2 );
+    
+    return 0;
+}
