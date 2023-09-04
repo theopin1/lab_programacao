848e016dccbed6776b5dcd3ed7a473b69e6ba74b
theopin1 <142269695+theopin1@users.noreply.github.com>
2023-09-04 08:46:40 -0300

    Create CPU.c

---
+++ CPU.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+float x, y, z, a, b, c, t;
+
+printf("Escreva os periodos dos tres eventos: ");
+scanf("%f%f%f", &x, &y, &z); 
+
+printf("Escreva os tempos de CPU dos tres eventos: ");
+scanf("%f%f%f", &a, &b, &c); 
+
+t = (a/x) + (b/y) + (c/z);
+
+if (t <= 1)
+
+printf("O sistema é escalonavel");
+
+else
+
+printf("O sistema não é escalonavel");
+
+return 0;
+}
