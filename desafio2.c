fb2ec878db74d2d159687a5ac437d42606177ba1
theopin1 <142269695+theopin1@users.noreply.github.com>
2023-11-30 23:50:59 -0300

    Update desafio2.c

--- desafio2.c
+++ desafio2.c
@@ -1,26 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define TAM 20
 #define RANGE 2
 
 struct Node{
 	int x;
-    int y;
+	int y;
+	char *c;
 	struct Node *prox;
 };
 
+void imprimir(struct Node *);
+
+void inserir(int, int, const char *, struct Node *);
+
 void gerarVet(char *);
 
-
 int main(){
-   char ram[TAM];
+	int tm = 1;
+	
+	struct Node h;
 
-   gerarVet(ram);
+    char ram[TAM];
 
+	srand(time(NULL));
 
-    return 0;
+	//system("clear");
+
+	h.prox = NULL;
+
+    gerarVet(ram);
+
+	for (int i = 1; i < TAM + 1; i++){
+		if (ram[i] == ram[i - 1]){
+			tm++;
+			 
+		} else if (ram[i - 1] == 1) {
+			 inserir(tm, (i - 1) - (tm - 1), "L", &h);
+            tm = 1;
+        } else if (ram[i - 1] == 0) {
+			 inserir(tm, (i - 1) - (tm - 1), "O", &h);
+            tm = 1;
+        }
+
+	}
+
+	imprimir(&h);
+
+	return 0;
+}
+
+void imprimir(struct Node *p){
+	struct Node *k;
+	
+	printf("---\n");
+	for (k=p->prox; k!=NULL; k=k->prox){
+		puts("\nO tamanho desse espaço é: ");
+		printf("%d\n",k->x);
+		puts("Endereço: ");
+		printf("%d\n",k->y);
+		puts("Esse endereço está:");
+		printf("%s\n",k->c);
+	}
+
+}
+
+void inserir(int v,int t, const char *h, struct Node *p){
+	struct Node *n;
+	n = malloc(sizeof(struct Node));
+	n->x = v;
+	n->y = t;
+	n->c = malloc(strlen(h) + 1);
+	strcpy(n->c, h);
+	n->prox = p->prox;
+	p->prox = n;
 }
 
 void gerarVet(char *r){
@@ -31,6 +87,8 @@ void gerarVet(char *r){
         r[i] = 1;
     }else 
         r[i] = 0;
+   
+   printf("%d",r[i]);
    }
 
 }
